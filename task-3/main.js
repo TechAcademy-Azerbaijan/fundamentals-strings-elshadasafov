41a4dcb8cc33383f053cb5b8b2b3c04fc878054e
elshadasafov <105041022+elshadasafov@users.noreply.github.com>
2022-06-28 11:38:04 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,24 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
+prompt.get("input", function (err, result) {
+  let str = result.input;
+  let a = 0;
+  let b = 0;
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  for (i = 0; i < str.length; i++) {
+    if (parseInt(str[i]) === 2) {
+      a = a + 1;
+    } else if (parseInt(str[i]) === 5) {
+      b = b + 1;
+    }
+  }
+
+  if (a > b) {
+    console.log("2");
+  } else if (a < b) {
+    console.log("5");
+  } else {
+    console.log("=");
+  }
 });
