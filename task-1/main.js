d5253e5da0bb0b21f7eac31ef6e467e999d18c13
elshadasafov <105041022+elshadasafov@users.noreply.github.com>
2022-06-28 10:30:36 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,12 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    let input = result.input; 
+    let numbers = input.split(','); 
+    let a = parseInt(numbers[0]); 
+    let b = parseInt(numbers[1]); 
+    let c = parseInt(numbers[2]); 
+
+    console.log(Math.min(Math.min(a,b),c));
 });
