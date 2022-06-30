9db432b67e1e792444724a7ed7994c13a3f82bf0
elshadasafov <105041022+elshadasafov@users.noreply.github.com>
2022-07-01 01:08:16 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,22 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
+prompt.get("input", function (err, result) {
+  //   let str = "Hello world! I learn to code."
+  let str = result.input;
+  let a = 0;
+  let b = 0;
+  let c = 0;
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === "+") {
+      a = a + 1;
+    } else if (str[i] === "-") {
+      b = b + 1;
+    } else if (str[i] === "*") {
+      c = c + 1;
+    }
+  }
+  let sum = a + b + c;
+  console.log(sum);
 });
